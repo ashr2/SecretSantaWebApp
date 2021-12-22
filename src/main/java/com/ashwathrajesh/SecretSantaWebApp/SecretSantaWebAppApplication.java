7a406e444ef6078a4824575861b426bec61e6313
ashrdev <ashrdev@gmail.com>
2021-12-22 13:12:32 -0500

    Initial commit

---
+++ src/main/java/com/ashwathrajesh/SecretSantaWebApp/SecretSantaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashwathrajesh.SecretSantaWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSantaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSantaWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashwathrajesh/SecretSantaWebApp/SecretSantaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashwathrajesh.SecretSantaWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
