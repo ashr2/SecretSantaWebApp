ce148af58bc388e0bdd177bb78aff5def21aede1
ashrdev <ashrdev@gmail.com>
2021-12-22 17:02:58 -0500

    Added Group Controller and group page to view

--- src/main/java/com/ashwathrajesh/SecretSantaWebApp/GroupController.java
+++ src/main/java/com/ashwathrajesh/SecretSantaWebApp/GroupController.java
@@ -1,6 +1,27 @@
 package com.ashwathrajesh.SecretSantaWebApp;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
+@Controller
 public class GroupController {
+	public static ArrayList<Person> createGroup() {
+		Group myGroup = new Group();
+		myGroup.addPerson(new Person("Person A"));
+		myGroup.addPerson(new Person("Person B"));
+		myGroup.addPerson(new Person("Person C"));
+		myGroup.addPerson(new Person("Person D"));
+		myGroup.assignPersons();
+		return(myGroup.getPersons());
+	}
 	
+	@GetMapping("/group")
+	public String group(Model model) {
+		ArrayList<Person> persons = new ArrayList<Person>();
+		model.addAttribute("persons", persons);
+		return "group";
+	}
 }
